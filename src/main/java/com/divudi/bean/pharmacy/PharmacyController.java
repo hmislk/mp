671897aab4b6ec62b391d4c784664497e7bf4abb
root <root@www.matarapharmacy.com>
2014-08-11 11:02:24 +0530

    Merge branch 'master' of http://divudi@bitbucket.org/divudi/pharmacy.git
    Conflicts:
    	src/main/resources/META-INF/persistence.xml
    	src/main/setup/glassfish-resources.xml

--- src/main/java/com/divudi/bean/pharmacy/PharmacyController.java
+++ src/main/java/com/divudi/bean/pharmacy/PharmacyController.java
@@ -84,6 +84,7 @@ public class PharmacyController implements Serializable {
     private List<BillItem> grns;
     private List<BillItem> pos;
     private List<BillItem> directPurchase;
+    double persentage;
 
     public void makeNull() {
         departmentSale = null;
@@ -269,7 +270,7 @@ public class PharmacyController implements Serializable {
         Map m = new HashMap();
         m.put("ins", institution);
         m.put("i", itm);
-        sql = "select i.department,sum(i.stock) from Stock i where "
+        sql = "select i.department,sum(i.stock),i.itemBatch.dateOfExpire,i.itemBatch.purcahseRate,i.itemBatch.retailsaleRate from Stock i where "
                 + " i.department.institution=:ins and i.itemBatch.item=:i"
                 + " group by i.department"
                 + " having sum(i.stock) > 0 ";
@@ -429,9 +430,9 @@ public class PharmacyController implements Serializable {
         List<Item> items = getItemController().getDealorItem();
         List<Institution> insList = getCompany();
         for (Item i : items) {
-            
+
             System.err.println("i.getName() = " + i.getName());
-            
+
             double itemStockTotal = 0;
             double itemAverageTotal = 0;
             StockAverage stockAverage = new StockAverage();
@@ -745,6 +746,211 @@ public class PharmacyController implements Serializable {
         return value;
     }
 
+    double totalStock = 0.0;
+    double totalPurchase = 0.0;
+    double totalRetailSale = 0.0;
+
+    public void createStockDealorAllDepartments() {
+
+        stockAverages = new ArrayList<>();
+        List<Item> items = getItemController().getDealorItem();
+        List<Institution> insList = getCompany();
+        for (Item i : items) {
+            double itemStockTotal = 0;
+            double itemPurchaaseTotal = 0;
+            double itemRetailSaleTotal = 0;
+            StockAverage stockAverage = new StockAverage();
+            stockAverage.setItem(i);
+            stockAverage.setInstitutionStocks(new ArrayList<InstitutionStock>());
+
+            for (Institution ins : insList) {
+                double insStockTotal = 0;
+                double insAverageTotal = 0;
+                double insPurchaseTotal = 0.0;
+                double insRetailSaleTotal = 0.0;
+                InstitutionStock newTable = new InstitutionStock();
+                newTable.setInstitution(ins);
+                newTable.setDepatmentStocks(new ArrayList<DepartmentStock>());
+                List<Object[]> objs = calDepartmentStock(ins, i);
+
+                for (Object[] obj : objs) {
+//                    System.err.println("Inside ");
+                    DepartmentStock r = new DepartmentStock();
+                    r.setDepartment((Department) obj[0]);
+                    r.setStock((Double) obj[1]);
+                    r.setExpiryDate((Date) obj[2]);
+                    r.setPurchaseRate((Double) obj[3]);
+                    r.setRetailSale((Double) obj[4]);
+
+                    insStockTotal += r.getStock();
+                    insPurchaseTotal += r.getPurchaseRate() * r.getStock();
+                    insRetailSaleTotal += r.getRetailSale() * r.getStock();
+                    newTable.getDepatmentStocks().add(r);
+
+                }
+
+                newTable.setInstitutionTotal(insStockTotal);
+                newTable.setInstitutionPurchaseTotal(insPurchaseTotal);
+                newTable.setInstitutionRetailSaleTotal(insRetailSaleTotal);
+
+                if (insStockTotal != 0 || insAverageTotal != 0) {
+                    stockAverage.getInstitutionStocks().add(newTable);
+                    itemStockTotal += insStockTotal;
+                    itemPurchaaseTotal += insPurchaseTotal;
+                    itemRetailSaleTotal += insRetailSaleTotal;
+                }
+            }
+
+            if (itemStockTotal != 0) {
+                stockAverage.setItemStockTotal(itemStockTotal);
+                stockAverage.setItemPurchaseTotal(itemPurchaaseTotal);
+                stockAverage.setItemRetailSalekTotal(itemRetailSaleTotal);
+                stockAverages.add(stockAverage);
+            }
+        }
+        totalStock = 0.0;
+        totalPurchase = 0.0;
+        totalRetailSale = 0.0;
+        for (StockAverage sa : stockAverages) {
+            totalStock += sa.getItemStockTotal();
+            totalPurchase += sa.getItemPurchaseTotal();
+            totalRetailSale += sa.getItemRetailSalekTotal();
+        }
+        System.out.println("totalStock = " + totalStock);
+        System.out.println("totalPurchase = " + totalPurchase);
+        System.out.println("totalRetailSale = " + totalRetailSale);
+    }
+
+    public void averageByDatePercentage() {
+        Calendar frm = Calendar.getInstance();
+        frm.setTime(fromDate);
+        Calendar to = Calendar.getInstance();
+        to.setTime(toDate);
+
+        long lValue = to.getTimeInMillis() - frm.getTimeInMillis();
+        double dayCount = 0;
+        if (lValue != 0) {
+            dayCount = lValue / (1000 * 60 * 60 * 24);
+        }
+
+        System.err.println("Day Count " + dayCount);
+        createStockAverageByPer(dayCount);
+
+    }
+
+    public void createStockAverageByPer(double dayCount) {
+
+        stockAverages = new ArrayList<>();
+        List<Item> items = getItemController().getDealorItem();
+        List<Institution> insList = getCompany();
+        for (Item i : items) {
+            double itemStockTotal = 0;
+            double itemAverageTotal = 0;
+            StockAverage stockAverage = new StockAverage();
+            stockAverage.setItem(i);
+            stockAverage.setInstitutionStocks(new ArrayList<InstitutionStock>());
+
+            for (Institution ins : insList) {
+                double insStockTotal = 0;
+                double insAverageTotal = 0;
+                double insStock = 0.0;
+                InstitutionStock newTable = new InstitutionStock();
+                newTable.setInstitution(ins);
+                newTable.setDepatmentStocks(new ArrayList<DepartmentStock>());
+                List<Object[]> objs = calDepartmentStock(ins, i);
+                double calPerStock = 0.0;
+                for (Object[] obj : objs) {
+//                    System.err.println("Inside ");
+                    DepartmentStock r = new DepartmentStock();
+                    r.setDepartment((Department) obj[0]);
+                    r.setStock((Double) obj[1]);
+
+                    double qty = calDepartmentSaleQtyByPer(r.getDepartment(), i);
+                    qty = 0 - qty;
+                    if (qty != 0 && dayCount != 0) {
+                        double avg = qty / dayCount;
+                        calPerStock = (avg * persentage) / 100;
+                        insStock = r.getStock();
+                        r.setAverage(avg);
+                    }
+
+//                    System.out.println("calPerStock = " + calPerStock);
+//                    System.out.println("insStockTotal = " + insStockTotal);
+//                    System.out.println("insAverageTotal = " + insAverageTotal);
+                    if ((insStock < calPerStock) && r.getStock() != 0) {
+                        System.out.println("*insStock = " + insStock);
+                        System.out.println("*calPerStock = " + calPerStock);
+                        insStockTotal += r.getStock();
+                        insAverageTotal += r.getAverage();
+                        newTable.getDepatmentStocks().add(r);
+                    }
+
+                }
+
+//                System.out.println("calPerStock = " + calPerStock);
+//                System.out.println("insStockTotal = " + insStockTotal);
+//                System.out.println("insAverageTotal = " + insAverageTotal);
+                newTable.setInstitutionTotal(insStockTotal);
+                newTable.setInstitutionAverage(insAverageTotal);
+
+                if ((insStockTotal != 0 || insAverageTotal != 0) && insStock < calPerStock) {
+                    stockAverage.getInstitutionStocks().add(newTable);
+                    itemStockTotal += insStockTotal;
+                    itemAverageTotal += insAverageTotal;
+                }
+            }
+
+            if (itemAverageTotal != 0 || itemStockTotal != 0) {
+                stockAverage.setItemAverageTotal(itemAverageTotal);
+                stockAverage.setItemStockTotal(itemStockTotal);
+                stockAverages.add(stockAverage);
+            }
+
+        }
+
+    }
+
+    public double calDepartmentSaleQtyByPer(Department department, Item itm) {
+
+        if (itm instanceof Ampp) {
+            itm = ((Ampp) pharmacyItem).getAmp();
+        }
+
+        String sql;
+        Map m = new HashMap();
+        m.put("itm", itm);
+        m.put("dep", department);
+        m.put("frm", getFromDate());
+        m.put("to", getToDate());
+        m.put("btp", BillType.PharmacyPre);
+        m.put("refType", BillType.PharmacySale);
+        sql = "select sum(i.pharmaceuticalBillItem.qty) "
+                + " from BillItem i where i.bill.department=:dep"
+                + " and i.bill.referenceBill.billType=:refType "
+                + " and i.item=:itm and i.bill.billType=:btp and "
+                + " i.createdAt between :frm and :to  ";
+
+        return getBillItemFacade().findDoubleByJpql(sql, m, TemporalType.TIMESTAMP);
+
+    }
+
+    public void averageByMonthByPercentage() {
+        Calendar frm = Calendar.getInstance();
+        frm.setTime(fromDate);
+        Calendar to = Calendar.getInstance();
+        to.setTime(toDate);
+
+        long lValue = to.getTimeInMillis() - frm.getTimeInMillis();
+        double monthCount = 0;
+        if (lValue != 0) {
+            monthCount = lValue / (1000 * 60 * 60 * 24 * 30);
+        }
+
+        System.err.println("Month Count " + monthCount);
+        createStockAverageByPer(Math.abs(monthCount));
+
+    }
+
     public SessionController getSessionController() {
         return sessionController;
     }
@@ -988,6 +1194,38 @@ public class PharmacyController implements Serializable {
 
     public void setItemController(ItemController itemController) {
         this.itemController = itemController;
+    }
+
+    public double getPersentage() {
+        return persentage;
+    }
+
+    public void setPersentage(double persentage) {
+        this.persentage = persentage;
+    }
+
+    public double getTotalStock() {
+        return totalStock;
+    }
+
+    public void setTotalStock(double totalStock) {
+        this.totalStock = totalStock;
+    }
+
+    public double getTotalPurchase() {
+        return totalPurchase;
+    }
+
+    public void setTotalPurchase(double totalPurchase) {
+        this.totalPurchase = totalPurchase;
+    }
+
+    public double getTotalRetailSale() {
+        return totalRetailSale;
+    }
+
+    public void setTotalRetailSale(double totalRetailSale) {
+        this.totalRetailSale = totalRetailSale;
     }
 
 }
